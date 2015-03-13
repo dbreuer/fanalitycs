b04f93b8422fe0aa8a412b31431377c443ad7eb6
David Breuer <dbreuer83@gmail.com>
2015-03-13 09:55:33 +0000

    init

---
+++ dist/modules/js/whiteframe/whiteframe.js
@@ -0,0 +1,15 @@
+/*!
+ * Angular Material Design
+ * https://github.com/angular/material
+ * @license MIT
+ * v0.8.3
+ */
+(function() {
+'use strict';
+
+/**
+ * @ngdoc module
+ * @name material.components.whiteframe
+ */
+angular.module('material.components.whiteframe', []);
+})();
